9d8abac137bb8068a402586f22b3adadc12f766a
PedroStabach <pedroaugustostabach3@gmail.com>
2025-04-09 21:19:36 -0300

    1 versao

---
+++ jokempo.py
@@ -0,0 +1,15 @@
+req = input("deseja Jogar? S/N")
+
+while true
+    if req != "S"
+        break
+    // opcao jogador x jogador 
+
+
+
+
+    // opcao jogador x bot
+
+
+    //opcao bot x bot
+    req = input("deseja continuar jogando?")
